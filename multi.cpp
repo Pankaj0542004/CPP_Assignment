b1bd7af6b20bccedfc1564dbfcdd26485f9566f2
pankaj marathe <pankajgmarathe12@gmail.com>
2023-04-19 21:13:29 +0530

    Hello

--- multi.cpp
+++ multi.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <iostream>
 class MultiplicationTable 
 {
@@ -29,3 +30,36 @@ int main()
 
     return 0;
 }
+=======
+#include <iostream>
+class MultiplicationTable 
+{
+public:
+    MultiplicationTable(int n) 
+	{
+        num = n;
+    }
+
+    void printTable() {
+        for (int i = 1; i <= 10; i++) 
+		{
+            std::cout << num << " x " << i << " = " << (num * i) << std::endl;
+        }
+    }
+
+private:
+    int num;
+};
+
+int main() 
+{
+    int n;
+    std::cout << "Enter a number: ";
+    std::cin >> n;
+
+    MultiplicationTable table(n);
+    table.printTable();
+
+    return 0;
+}
+>>>>>>> 71b0133 (Hello)
