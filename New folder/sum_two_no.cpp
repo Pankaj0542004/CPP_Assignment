e8219688d923742c708feea045a6ce130cdfb906
pankaj marathe <pankajgmarathe12@gmail.com>
2023-04-19 21:29:30 +0530

    p

---
+++ New folder/sum_two_no.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,sum;
+
+    cout<<"Enter first no:";
+    cin>>a;
+
+    cout<<"Enter second no:";
+    cin>>b;
+
+    cout<<"sum of two no is:"<<a+b;
+
+    return 0;
+
+}
